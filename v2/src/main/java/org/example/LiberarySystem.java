2e1e4975ffc3f7d4893fad6b49a683e48c069cb0
ciancarmody <101117001+ciancarmody@users.noreply.github.com>
2022-11-13 14:46:46 +0000

    Update LiberarySystem.java

--- v2/src/main/java/org/example/LiberarySystem.java
+++ v2/src/main/java/org/example/LiberarySystem.java
@@ -1,11 +1,65 @@
 package org.example;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class LiberarySystem{
     public static String in;
     private BookList masterList = new BookList();
 
+    
+    
+    public void registerUser{
+
+public static void main(String[] args) 
+    {
+    Person p = new Person();
+
+  public static string emailCheck(String email) {
+    if(!email.contains("@") || !email.contains(".") || !email.matches(".*[a-zA-Z].*")){//makes sure email is an email
+        throw new emailException("Cannot add this email. That's not an email");
+    return emailCheck;
+
+  }
+
+
+  //scanner starts
+  public static void main(String[] args) { 
+    Scanner input = new Scanner(System.in);
+
+    //enter name, email, department(s)
+    System.out.print("Enter Name and Email: ");
+    String name = input.next();
+    String email = input.next();
+    String departments = input.next(); 
+
+
+    try {
+      this.name = p.name;
+      this.email = p.email;
+      String[] arr = p.departments.split(", ");//splits up departments string and adds to arraylist
+      this.departments.addAll(Arrays.asList(arr));
+
+
+
+    }
+    catch (emailException ex) {
+      System.out.println("Please try again.");
+    }
+
+    System.out.println("Execution continues ...");
+  }
+}
+}
+}
+    
+    
+    
+    
+    
+    
+    
+    
     public void addBook(Book book){
         masterList.addBook(book);
     }
